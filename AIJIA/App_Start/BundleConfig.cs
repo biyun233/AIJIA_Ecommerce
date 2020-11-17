bec7b3448fbc591b63c3e71e1a99715b3c9b7c2c
dialloDioulde <diouldemariia@hotmail.fr>
2020-11-17 16:04:06 +0100

    debug

--- AIJIA/App_Start/BundleConfig.cs
+++ AIJIA/App_Start/BundleConfig.cs
@@ -20,14 +20,11 @@ namespace AIJIA
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                       "~/Scripts/admin/admin.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/Site.css",
-                       "~/Content/Admin/Admin.css",
-                        "~/Content/Admin/Index.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
